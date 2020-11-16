f01d13dd9da703411f3ed51af0471f0949069ba4
Mohan Kumar <mohanrathore98@gmail.com>
2020-11-16 15:01:33 +0530

    Loops in pyhton

---
+++ conditional_Statment.py
@@ -0,0 +1,30 @@
+# IF ELSE IN PYTHON
+var1 = 223
+var2 = 223
+if var1>var2 :
+    print("greater")
+elif var1==var2:
+    print("equal")
+else:
+    print("lesser")
+
+list = [2,3,4,5,6]
+
+if 15 in list:
+    print("Yes present in list")
+
+# FOR LOOP IN PYTHON
+list1 = [["mohan",2],["kumarf",2],["rathore",4],["tanisha",5],["surabhi",7]]
+dict1 =dict(list1)
+print(dict1)
+for item,lollypop in dict1.items():
+    print(item,lollypop)
+
+
+# WHILE LOOP IN PYTHON
+i=0
+while i<45:
+    print(1)
+    i+=10
+
+# BREAK AND CONTINUE STATMENT IN PYTHON
