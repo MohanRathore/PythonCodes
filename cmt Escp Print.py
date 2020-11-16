0b78a8b82d4ffe81a46799988311503d7bf61721
Mohan Kumar <mohanrathore98@gmail.com>
2020-11-16 11:52:16 +0530

    Comment Escape Print Statement

---
+++ cmt Escp Print.py
@@ -0,0 +1,15 @@
+#print statment
+print("Hello World")
+print("12345678")
+
+# Comments in python
+# next comment
+"""This is a 
+multi line
+comment in python"""
+
+#Escape character in pyhton
+print("mohan\\nkumar",end="") #for prininting in same line or any other keyword 
+print("mohan\"kumar\"")
+print("mohan\'kumar\'")
+print("mohan\\tkumar\\t")
